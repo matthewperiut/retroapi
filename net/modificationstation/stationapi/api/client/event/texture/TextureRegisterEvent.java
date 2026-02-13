aa61bbd40384829961b69aded59ce209864b0c00
matthewperiut <matthewperiut@gmail.com>
2026-02-12 19:05:05 -0500

    cleanup

--- net/modificationstation/stationapi/api/client/event/texture/TextureRegisterEvent.java
+++
@@ -1,7 +0,0 @@
-package net.modificationstation.stationapi.api.client.event.texture;
-
-import lombok.experimental.SuperBuilder;
-import net.mine_diver.unsafeevents.Event;
-
-@SuperBuilder
-public class TextureRegisterEvent extends Event {}
